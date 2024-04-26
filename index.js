db1e4689228c23da460b7384d0c3cbc54bdfe952
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-04-26 20:32:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.insvee7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const themeCollection = client.db('Tourista').collection('theme');
+
+    // app.post('/theme', async(req, res) => {
+    //     const data = {theme : "light"};
+    //     const result = await themeCollection.insertOne(data);
+        
+    //     res.send(result);
+    // })
+
+    app.get('/theme', async(req, res) => {
+        const result  = await themeCollection.find().toArray();
+        res.send(result)
+        
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Travel all over the world!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

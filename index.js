e396e8abdaa2c9e12430d6f87b167a12a6f58fa0
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-04-27 22:25:53 +0600

    delete api is created

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/mySpots/:email', async(req, res) => {
+            const email =  req.params.email;
+            const query = {email : email};
+            const result = await touristSpotsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/spot/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await touristSpotsCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
